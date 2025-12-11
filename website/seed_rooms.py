f8aa9e3897e7ab72e25ae83257c07716020a0ff8
Emely Pozo-Lin <yystudent6@gmail.com>
2025-12-11 01:54:25 -0500

    Revert "Revert the last commit"

--- website/seed_rooms.py
+++ website/seed_rooms.py
@@ -1,8 +1,9 @@
-from app import app, db
-from models import Room
+from website import db
+from . import website
+from .models import Room
 
 def seed_rooms():
-    with app.app_context():
+    with website.app_context():
         # Check if rooms already exist
         if Room.query.first():
             print("Rooms already exist in database")
